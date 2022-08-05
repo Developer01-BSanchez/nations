9583d41fe6031816016cf91f8a8950b5c171bad5
bsanchez <bysanchez84@misena.edu.co>
2022-08-05 11:37:43 -0500

    Se realizaron las relaciones entre los models Continent, Country y Region

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,18 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Continent extends Model
+class Region extends Model
 {
     //tabla con la cual el modelo se relaciona dentro de la clase
-    protected $table = "continents";
+    protected $table = "regions";
     //calve primaria de la tabla
-    protected $primaryKey = "continent_id";
+    protected $primaryKey = "region_id";
     //anular campos de auditoria
     public $timestamps = false;
     use HasFactory;
 
-    //relacion entre continente y región
-    public function regiones(){
-        return $this->hasMany( Region::class , 'continent_id');
+    //relacion entre region y paises 1:m
+    public function paises(){
+        return $this->hasMany(Country::class,'region_id');
     }
 }
